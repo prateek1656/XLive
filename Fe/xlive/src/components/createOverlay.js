c4d0d68a42cdd23ee9be4ad852bdbff83989d8e3
prateek <prateekyadav1656@gmail.com>
2023-09-30 20:04:41 +0530

    added fe

--- Fe/xlive/src/components/createOverlay.js
+++ Fe/xlive/src/components/createOverlay.js
@@ -1,8 +1,7 @@
-// OverlayForm.js
-import React, { useState } from 'react';
-import './OverlayForm.css';
+import React, { useState, useRef } from 'react';
+import './AddOverlay.css';
 
-function OverlayForm() {
+function CreateOverlays() {
   const [overlay, setOverlay] = useState({
     name: '',
     content: '',
@@ -10,27 +9,58 @@ function OverlayForm() {
     position_y: 0,
   });
 
+  const [isDragging, setIsDragging] = useState(false);
+  const overlayRef = useRef(null);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setOverlay({ ...overlay, [name]: value });
   };
 
-  const handleMouseOver = (e) => {
-    // Update overlay position and display coordinates
-    const rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    setOverlay({ ...overlay, position_x: x, position_y: y });
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
   };
 
-  const handleSubmit = (e) => {
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      const rect = overlayRef.current.getBoundingClientRect();
+      const x = e.clientX ;
+      const y = e.clientY ;
+    //   const x = e.clientX - rect.width / 2;
+    //   const y = e.clientY - rect.height / 2;
+      setOverlay({ ...overlay, position_x: x, position_y: y });
+    }
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Send overlay data to your backend API
-    console.log('Overlay Data:', overlay);
+    try {
+      const response = await fetch('http://127.0.0.1:5001/overlays', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(overlay),
+      });
+      if (response.ok) {
+        // Overlay created successfully, handle the response as needed
+        console.log('Overlay created successfully');
+      } else {
+        // Handle any error response
+        console.error('Error creating overlay');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
-    <div className="overlay-form">
+    <div className="create-overlays">
       <h2>Create Overlay</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
@@ -53,7 +83,13 @@ function OverlayForm() {
             onChange={handleChange}
           />
         </div>
-        <div className="overlay-preview" onMouseMove={handleMouseOver}>
+        <div
+          className="overlay-preview"
+          ref={overlayRef}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseMove={handleMouseMove}
+        >
           <div
             className="overlay"
             style={{ left: overlay.position_x, top: overlay.position_y }}
@@ -70,4 +106,4 @@ function OverlayForm() {
   );
 }
 
-export default OverlayForm;
+export default CreateOverlays;
